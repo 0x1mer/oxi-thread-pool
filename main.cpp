60055f08daebab29494eb1af0b09024ec45cceeb
0x1mer <oximeracer@gmail.com>
2025-12-26 05:44:34 +0200

    delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#define LOGGING
-#include "ThreadOxPool/ThreadPool.h"
-
-int main(int, char**){
-    std::cout << "Hello, from OxiThreadPool!\n";
-    ThreadPool* pool = new ThreadPool();
-}
